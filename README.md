43ef5d0b846ee6b9216cb0791ebfe625fbd96ec0
yyubin <hazing120@gmail.com>
2023-05-06 22:22:49 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Kotlin Spring
+
+* Kotlin
+* JDK 17
+* Spring boot
+
+### References
+https://kotlinlang.org/docs/jvm-spring-boot-add-data-class.html
---
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -0,0 +1,20 @@
+package com.example.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoApplication>(*args)
+}
+
+@RestController
+class MessageController {
+    @GetMapping("/")
+    fun index(@RequestParam("name") name: String) = "Hello, $name!"
+}
